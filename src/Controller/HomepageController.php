65902f953cc39843b3e9aa5771fd9e17e9696b2e
Nick Andreev <bakasaru@list.ru>
2018-09-03 22:41:59 +0300

    Played with controllers and Twig templates

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+
+    /**
+     * @Route("/books/{name}")
+     */
+    public function show($name)
+    {
+        return $this->render('homepage/book.html.twig', [
+            'name' => $name
+        ]);
+    }
+}
